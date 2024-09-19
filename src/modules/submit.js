5579b75899eb20943ea4f015de38046f16f8f198
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-09-18 21:30:47 -0300

    Buscando usuario no back-end

--- src/modules/submit.js
+++ src/modules/submit.js
@@ -1,9 +1,39 @@
+import { apiConfig } from "../services/api-config.js"
+
 const form = document.querySelector("form")
 const input = document.querySelector("input")
+const userInfo = document.getElementById("user-info")
 
 
-form.onsubmit = (event) => {
-  event.preventDefault()
+form.onsubmit = async (event) => {
   
-  console.log(input.value)
+  event.preventDefault()
+  const response = await fetch(`${apiConfig.baseURL}/clients`)
+  
+  const clients = await response.json()
+
+  const idInput = input.value
+
+  console.log(idInput)
+  console.log(clients)
+
+  await clients.forEach(({ id, name, clientSince }) => {
+    if (idInput === id) {
+      const h1 = document.createElement("h1")
+      const p = document.createElement("p")
+      //div.setAttribute("id", "user-info")
+
+      console.log(id)
+      console.log(name)
+      console.log(clientSince)
+      h1.textContent = name
+      p.textContent = clientSince
+
+      userInfo.innerHTML = ""
+      userInfo.append(h1)
+      userInfo.append(p)
+    }else{
+      console.log("erro!!")
+    }
+  });
 }
