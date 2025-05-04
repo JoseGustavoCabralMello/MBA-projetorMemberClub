46327a978270e101fc98c00979fea2e53c8c2d33
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-05-04 12:10:08 -0300

    getting userName

--- src/modules/submit.js
+++ src/modules/submit.js
@@ -4,36 +4,25 @@ const form = document.querySelector("form")
 const input = document.querySelector("input")
 const userInfo = document.getElementById("user-info")
 
-
 form.onsubmit = async (event) => {
   
   event.preventDefault()
   const response = await fetch(`${apiConfig.baseURL}/clients`)
-  
   const clients = await response.json()
-
   const idInput = input.value
 
-  console.log(idInput)
-  console.log(clients)
+  // console.log(idInput)
+  // console.log(clients)
 
-  await clients.forEach(({ id, name, clientSince }) => {
-    if (idInput === id) {
-      const h1 = document.createElement("h1")
-      const p = document.createElement("p")
-      //div.setAttribute("id", "user-info")
-
-      console.log(id)
-      console.log(name)
-      console.log(clientSince)
-      h1.textContent = name
-      p.textContent = clientSince
-
-      userInfo.innerHTML = ""
-      userInfo.append(h1)
-      userInfo.append(p)
-    }else{
-      console.log("erro!!")
-    }
-  });
+  const client = clients.filter(client => (client.id === idInput))
+  // console.log(client)
+  
+  if (client.length === 0) {
+    alert("Insira um ID válido!")
+  } else {
+    const clientId = client[0].id
+    console.log(clientId)
+    
+    return(clientId)
+  }
 }
