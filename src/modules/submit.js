aa984e75998502679e0aa84b3650a3de6e17ff5b
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-05-05 06:42:53 -0300

    history title cuts

--- src/modules/submit.js
+++ src/modules/submit.js
@@ -4,6 +4,7 @@ const form = document.querySelector("form")
 const input = document.querySelector("input")
 const user = document.getElementById("user-info")
 const avatar = document.getElementById("avatar")
+const historyP = document.getElementById("history-p")
 
 form.onsubmit = async (event) => {
   
@@ -12,40 +13,25 @@ form.onsubmit = async (event) => {
   const clients = await response.json()
   const idInput = input.value
 
-  // console.log(idInput)
-  // console.log(clients)
-
   const client = clients.filter(client => (client.id === idInput))
-  console.log(client)
   
   if (client.length === 0) {
     alert("Insira um ID válido!")
   } else {
-    const clientId = client[0].name
-    console.log(clientId)
-    
     updateUser(client)
-    return(clientId)
+    updateHistory(client)
   }
 }
 
-async function updateUser (client) {
+function updateUser (client) {
   user.innerHTML = ""
   avatar.innerHTML = ""
-
-  console.log(client[0].appointmentHistory.length)
-
+  
   const avatarUser = document.createElement("div")
   avatarUser.setAttribute("id", "avatar")
 
   const avatarImg = document.createElement("img")
-
-  // const path = `./src/assets/${client[0].id}.png`
-  // console.log(path)
-
   avatarImg.setAttribute("src", `./src/assets/${client[0].id}.png`)
-  console.log(avatarImg)
-
   avatarImg.setAttribute("alt", "avatar")
   avatar.append(avatarImg)
 
@@ -54,11 +40,21 @@ async function updateUser (client) {
   
   const userInfoH1 = document.createElement("h1")
   userInfoH1.textContent = client[0].name
-  console.log(userInfoH1)
 
   const userInfoP = document.createElement("p")
   userInfoP.textContent = "Cliente desde " + client[0].clientSince
-  console.log(userInfoP)
   
   user.append(userInfoH1, userInfoP)
+}
+
+function updateHistory(client) {
+  historyP.innerHTML = ""
+
+  const userhistoryP= document.createElement("p")
+  userhistoryP.setAttribute("id", "history-p")
+  userhistoryP.textContent = client[0].appointmentHistory.length+" cortes"
+
+  historyP.append(userhistoryP)
+
+  console.log(client[0].appointmentHistory.length)
 }
